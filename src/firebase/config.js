6c10d66c2f90a1e219b9fdc0b4ab608541edf168
Yazeed Alfuqaha <yalfuqaha8@gmail.com>
2026-03-12 11:38:43 +0300

    hide firebase config and update gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,12 @@ import 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAbZtTHqCzGJZCpFu6a6ZQdgDrXwVgOyT8",
-  authDomain: "vue-firebase-a3e9f.firebaseapp.com",
-  projectId: "vue-firebase-a3e9f",
-  storageBucket: "vue-firebase-a3e9f.firebasestorage.app",
-  messagingSenderId: "451733548701",
-  appId: "1:451733548701:web:a35cef62620cdfbafeee74",
-  measurementId: "G-KQEV7HZ4TW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // init firebase
